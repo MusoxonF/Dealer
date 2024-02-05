68be7706472b71113d73d8f6a0efbbfa0c18c12f
Musoxon <orinboyevmusoxon05@gamil.com>
2024-02-05 15:32:37 +0500

    talab, taklif full model created

--- taklif/models.py
+++ taklif/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from user.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Photo(models.Model):
+    Image = models.ImageField(upload_to = 'talab_photos', verbose_name='maxsulot photo')
+
+
+class Tag(models.Model):
+    Tag = models.TextField(verbose_name='tag')
+
+
+class Talab(models.Model):
+    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name='talab_users')
+    photo = models.ManyToManyField(Photo, related_name='talab_photo', null=True, blank=True)
+    video = models.FileField(upload_to = 'talab_files', null=True, blank=True)
+    title = models.CharField(max_length=255)
+    batafsil = models.TextField(max_length=255, blank=True, null=True, verbose_name='batafsil')
+    min_price = models.IntegerField(validators=[MinValueValidator(0)], verbose_name = 'necha puldan boshlansin')
+    max_price = models.IntegerField(validators=[MinValueValidator(0)], verbose_name = 'necha pulgacha bo\'lsin')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class CommentPhoto(models.Model):
+    image = models.ImageField(upload_to = 'comment_photos', verbose_name='maxsulot photo')
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name='comment_users')
+    talab = models.ForeignKey(Talab, on_delete = models.CASCADE)
+    comment = models.TextField(verbose_name='comment')
+    photo = models.ManyToManyField(CommentPhoto, related_name = 'Comment_Photo', null=True, blank=True)
+    video = models.FileField(upload_to = 'comment_files', null = True, blank = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment
+    
+
--- talab/models.py
+++ talab/models.py
@@ -14,17 +14,20 @@ class Tag(models.Model):
 class Taklif(models.Model):
     author = models.ForeignKey(User, on_delete = models.CASCADE)
     title = models.CharField(max_length=255)
-    batafsil = models.TextField(max_length=255)
+    batafsil = models.TextField(max_length=255, null=True, blank=True)
     photo = models.ManyToManyField(Photo, related_name='taklif_photo')
     video = models.FileField(upload_to='taklif_videos/', blank=True, null=True)
     tag = models.ManyToManyField(Tag, related_name='taklif_tag')
-    price = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    price = models.IntegerField(validators=[MinValueValidator(0)])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.title
-        
+
+
+class OtzivPhoto(models.Model):
+    image = models.ImageField(upload_to='otziv_photos/')
 
 
 class Otziv(models.Model):
@@ -38,6 +41,8 @@ class Otziv(models.Model):
     status = models.CharField(max_length=6, choices=STATUS, default = '5')
     comment = models.TextField(verbose_name = 'Izoh')
     taklif = models.ForeignKey(Taklif, on_delete = models.CASCADE)
+    video = models.FileField(upload_to='otziv_files/', blank=True, null=True)
+    photo = models.ManyToManyField(OtzivPhoto, related_name='otziv_photo')
 
     def __str__(self):
         return self.taklif.title
